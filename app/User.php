8cf4e115d6d0a9e35737354055f4116b2f9e0552
Tomonori Ueno <t.uepon@gmail.com>
2018-08-23 23:48:46 +0900

    Dashboard User Listings

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // Add One to many relationship
+    public function listings(){
+        return $this->hasMany('App\Listing'); 
+    }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,13 +8,23 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <h3>Your Listings</h3>
+                    @if(count($listings))
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Company</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach($listings as $listing)
+                                <tr>
+                                  <td>{{$listing->name}}</td>
+                                  <td></td>
+                                  <td></td>                               
+                                </tr>
+                            @endforeach
+                        </table>
                     @endif
-
-                    You are logged in!
                 </div>
             </div>
         </div>

c7d794ce9a4fab56d21aa4e36f115c117664e086
Tomonori Ueno <t.uepon@gmail.com>
2018-08-26 00:30:10 +0900

    edit listing added

---
+++ resources/views/layouts/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Edit Listing</div>
+
+            <div class="card-body">
+                {!!Form::open(['action' => 'ListingsController@update', 'method' => 'POST'])!!}
+                    {{Form::bsText('name', '', ['placeholder' => 'Company Name'])}}
+                    {{Form::bsText('website', '', ['placeholder' => 'Company Website'])}}
+                    {{Form::bsText('email', '', ['placeholder' => 'Contact Email'])}}
+                    {{Form::bsText('phone', '', ['placeholder' => 'Contact Phone'])}}
+                    {{Form::bsText('address', '', ['placeholder' => 'Business Address'])}}
+                    {{Form::bsTextArea('bio', '', ['placeholder' => 'About This Business'])}}
+                    {{Form::hidden('_method', 'PUT')}}
+                    {{Form::bsSubmit('submit')}}
+                {!!Form::close()!!}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
